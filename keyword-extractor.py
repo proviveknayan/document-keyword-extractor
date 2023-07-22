b1d88a4500041e0bab8c7f8969094e80c80b66cf
proviveknayan <proviveknayan@gmail.com>
2023-07-22 07:18:20 +0530

    add in-line comments in .py

--- keyword-extractor.py
+++ keyword-extractor.py
@@ -16,13 +16,21 @@ while count < numberOfPages:
     pageObject = pdfReadable.pages[count]
     count +=1
     text += pageObject.extract_text()
-    
+
+# because PyPDF2 cannot read scanned files, check if it returned words    
 if text != "":
     text = text
 
+# if not, run the textract to convert scanned/image based PDF files into text
 else:
     text = textract.process(filename,method='tesseract',language='eng')
 
+# execute the following two lines of code if using stopwords from the NLTK package for the first time
+# import nltk 
+# nltk.download('stopwords')
+
+# to use the stopwords, load them from the package by executing the following line of code
+# from nltk.corpus import stopwords
 r = Rake()
 r.extract_keywords_from_text(text)
 
